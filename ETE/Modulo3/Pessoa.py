d463db29db79182b7bd36382cf0ccf296213f838
Bowie <vanmaiasf@gmail.com>
2024-08-28 21:32:14 -0300

    Adicionado Modulo 3

--- ETE/Modulo3/Pessoa.py
+++ ETE/Modulo3/Pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
     __nome = str
-    def __init__(self, nome):
-        self.__nome = nome
+    def __init__(self):
+        pass
 
-p1 = Pessoa('Maria')
+        def setNome(self, nome):
+            self.__nome = nome
+        def getNome(self):
+            return self.__nome
+
+p1 = Pessoa()
+p1.setNome =("Jose")
 
 print(p1.__nome)
