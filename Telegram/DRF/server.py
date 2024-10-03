07fbb8566e6034b94e1cb9f12cfff9f3523fa847
Bowie <vanmaiasf@gmail.com>
2024-10-03 00:27:24 -0300

    Exibindo lista iterável

--- Telegram/DRF/server.py
+++ Telegram/DRF/server.py
@@ -1,2 +1,34 @@
-from http.server import HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html; charset=utf-8")
+            self.end_headers()
+            data = f"""
+            <html>
+                <head>
+                    <title> Olá, mundo!</title>
+                </head>
+                <body>
+                    <p>Testando o servidor HTTP!</p>
+                    <p>Diretório: {self.path}</p>
+                </body>
+            </html>
+            """.encode()
+            self.wfile.write(data)
+
+        elif self.path == "/eventos":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html; charset=utf-8")
+            self.end_headers()
+            data = f"""
+            <html>
+                <p> OI </p>
+            </html>
+            """.encode()
+            self.wfile.write(data)
+
+server = HTTPServer(('localhost', 8000), SimpleHandler)
+server.serve_forever()
