50a47b5e4ccd9e17be5ebcb27e64031df7dc59f1
Bowie <vanmaiasf@gmail.com>
2025-01-21 11:08:42 -0300

    Python - Map

---
+++ Geek University/map.py
@@ -0,0 +1,35 @@
+"""
+Map
+
+Com map, fazemos mapeamento de valores para função
+
+"""
+
+import math
+
+def area(r):
+    return math.pi * (r ** 2)
+
+print(area(2))
+print(area(5.3))
+
+raios = [2, 5, 7.1, 0.3, 10, 44]
+
+# Forma comum
+areas = []
+for r in raios:
+    areas.append((area(r)))
+
+    print(areas)
+
+# Forma 2 - map
+
+areas = map(area, raios)
+
+print(areas)
+print(type(areas))
+
+print(list(areas))
+
+# Forma 3 - Map com Lambda
+print(list(map(lambda r: math.pi * (r ** 2), raios)))
