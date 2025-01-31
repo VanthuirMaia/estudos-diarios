1bb4d929447bce4e08155938f37d5ce926ceb4ce
Bowie <vanmaiasf@gmail.com>
2025-01-30 22:23:17 -0300

    Python - POO

---
+++ DSA/Python/Python_Analise_Dados.py
@@ -0,0 +1,186 @@
+"""
+# Criando uma classe chamada Livro
+class Livro():
+    def __init__(self):
+        self.titulo = 'Sapiens - Uma Breve História da Humanidade'
+        self.isbn = 9988888
+        print('Construtor chamado para criar um objeto desta classe.')
+    
+    def imprime(self):
+        print("Foi criado o livro %s com ISBN %d" %(self.titulo, self.isbn))
+
+# Criando uma instância da classe Livro
+Livro1 = Livro()
+
+Livro1.titulo
+
+Livro1.imprime()
+
+# Criando uma classe chamada Livro
+class Livro():
+    def __init__(self, titulo, isbn):
+        self.titulo = titulo
+        self.isbn = isbn
+        print('Construtor chamado para criar um objeto desta classe.')
+    
+    def imprime(self, titulo, isbn):
+        print("Foi criado o livro %s com ISBN %d" %(self.titulo, self.isbn))
+
+# Criando uma instância da classe Livro
+Livro2 = Livro("O Poder do Hábito", 77886611)
+
+Livro2.titulo
+
+Livro2.imprime("O Poder do Hábito", 77886611)
+
+class Algoritmo():
+
+    def __init__(self, tipo_algo):
+        self.tipo = tipo_algo
+        print("Construtor chamado para criar um objeto desta classe.")
+# Criando objetos a partir da classe
+algo1 = Algoritmo(tipo_algo='Random Forest')
+algo2 = Algoritmo(tipo_algo='Deep Learning')
+
+# Atributos da classe
+algo1.tipo
+algo2.tipo
+
+--------------------------------------------------------------------------------------------
+# Criando uma classe
+class Funcionarios:
+    def __init__(self, nome, salario, cargo):
+        self.nome = nome
+        self.salario = salario
+        self.cargo = cargo
+
+    def listFunc(self):
+        print("Funcionario(a) " + self.nome + " tem salário de R$ " + str(self.salario) + " e o cargo é " + self.cargo)
+
+# Ciando um objeto chamado Func1 a partir da classe funcionarios
+Func1 = Funcionarios("Mary", 20000, "Cientista de Dados")
+
+# Usando o método da classe
+Func1.listFunc()
+
+
+print("**** Usando atributos ****")
+hasattr(Func1, "nome")
+hasattr(Func1, "salario")
+setattr(Func1, "salario", 4500)
+hasattr(Func1, "salario")
+getattr(Func1, "salario")
+delattr(Func1, "salario")
+hasattr(Func1, "salario")
+
+--------------------------------------------------------------------------------------------
+# Herança
+
+# Criando a classe Animal - Super Classe
+class Animal():
+    def __init__(self):
+        print("Animal Criado.")
+    
+    def imprimir(self):
+        print("Este é um animal.")
+
+    def comer(self):
+        print("Hora de comer.")
+
+    def emitir_som(self):
+        pass
+
+# Criando  classe Cachorro - Sub Classe
+class Cachorro(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Objeto cachorro criado.")
+    
+    def emitir_som(self):
+        print("Au au!")
+
+# Criando  classe Gato - Sub Classe
+class Gato(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Objeto Gato criado.")
+    
+    def emitir_som(self):
+        print("Miau!")
+
+# Criando um objeto (Instanciando a classe)
+rex = Cachorro()
+
+# Criando um objeto (Instanciando a classe)
+zeze = Gato()
+
+rex.emitir_som()
+
+zeze.emitir_som()
+
+rex.imprimir()
+
+rex.comer()
+
+zeze.comer()
+
+--------------------------------------------------------------------------------------------
+# Polimorfismo
+
+# SuperClasse
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def acelerar(self):
+        pass
+
+    def frear(self):
+        pass
+
+# SubClasse
+class Carro(Veiculo):
+
+    def acelerar(self):
+        print("O carro está acelerando")
+
+    def frear(self):
+        print("O carro esta freando")
+
+# SubClasse
+class Moto(Veiculo):
+
+    def acelerar(self):
+        print("A moto está acelerando")
+
+    def frear(self):
+        print("A moto esta freando")
+
+# SubClasse
+class Aviao(Veiculo):
+
+    def acelerar(self):
+        print("O Avião está acelerando")
+
+    def frear(self):
+        print("O avião esta freando")
+    
+    def decolar(self):
+        print("O avião esta decolando")
+
+lista_veiculos = [Carro("Porsche", "911 Turbo"), Moto("Honda", "CB 1000% Black Edition"), Aviao("Boeing", "757")]
+
+for item in lista_veiculos:
+    item.acelerar()
+    item.frear()
+    if isinstance(item, Aviao):
+        item.decolar()
+
+    print("-------------------------------------------------------")
+
+--------------------------------------------------------------------------------------------
+
+
+"""
+
